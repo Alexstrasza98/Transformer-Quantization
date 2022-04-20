777b71a0e6f76aaa94bcb36f0249047b431db80b
Alextrasza98 <yzx19981019@126.com>
2022-04-20 13:53:55 -0400

    load dataset, model definition, basic train func

---
+++ training.py
@@ -0,0 +1,128 @@
+import torch
+import torch.backends.cudnn as cudnn
+from tqdm import tqdm
+from utils.train_utils import AverageMeter, accuracy
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class Learner():
+    def __init__(self, train_config):
+        '''
+        task: define which task to train
+        train_conig: a dict of {'model': model to use,
+                                'loss_fn': loss function to use,
+                                'optim': optimizer to use,
+                                'datasets': dataset to use,
+                                'epochs': how many epochs to train on}
+        '''
+        # training settings
+        self.config = train_config
+        
+        self.model = train_config['model']
+        self.model_name = self.model.model_name
+        self.model.to(device)
+        self.criterion = train_config['loss_fn']
+        self.optimizer = train_config['optim']
+        
+        self.train_loader, self.test_loader = train_config['datasets']
+        
+        # initialization of record variables
+        self.test_acc_all = []
+        self.best_acc = 0.0
+        self.model_path = f'./res/{self.model_name}_best.pth'
+        
+    def train(self):
+        cudnn.benchmark = True
+        epochs = self.config['epochs']
+        
+        for epoch in tqdm(range(epochs)):
+            print('current lr {:.5e}'.format(self.optimizer.param_groups[0]['lr']))
+            
+            self.model.train()
+            self.train_step(epoch)
+            
+            self.model.eval()
+            test_acc = self.validate(epoch)
+            
+            self.test_acc_all.append(test_acc)
+            self.scheduler.step()
+            
+            if test_acc > self.best_acc:
+                self.save_model()
+                self.best_acc = test_acc
+            
+        return self.test_acc_all
+    
+    def train_step(self, epoch, verbose=True):
+        """
+        Run one train epoch
+        """
+        losses = AverageMeter()
+        top1_acc = AverageMeter()
+        
+        for i, (tokens, labels, masks) in enumerate(self.train_loader):
+            
+            # fetch batch data
+            labels = labels.to(device) - 1
+            tokens = tokens.to(device)
+            masks = masks.to(device)
+            
+            # compute output
+            logits = self.model(tokens, masks.view(masks.shape[0],1,1,masks.shape[1]))
+            loss = self.criterion(logits, labels)
+
+            # compute gradient and do SGD step
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+            # measure accuracy and record loss
+            prec1 = accuracy(logits.data, labels)[0]
+            losses.update(loss.item(), labels.size(0))
+            top1_acc.update(prec1.item(), labels.size(0))
+            
+            
+            # measure elapsed time
+            if i % 100 == 0 and verbose:
+                print('Epoch: [{0}][{1}/{2}]\t'
+                      'Loss {loss.avg:.4f}\t'
+                      'Prec@1 {top1.avg:.3f}'.format(
+                          epoch, i, i, loss=losses, top1=top1_acc))
+                
+    def validate(self, epoch, verbose=True):
+        """
+        Run evaluation
+        """
+        top1_acc = AverageMeter()
+
+        with torch.no_grad():
+            for tokens, labels, masks in self.test_loader:
+                
+                # fetch batch data
+                labels = labels.to(device) - 1
+                tokens = tokens.to(device)
+                masks = masks.to(device)
+
+                # compute output
+                logits = self.model(tokens, masks.view(masks.shape[0],1,1,masks.shape[1]))
+
+                # measure accuracy and record loss
+                prec1 = accuracy(logits.data, labels)[0]
+                top1_acc.update(prec1.item(), labels.size(0))
+        if verbose:
+            print('Epoch[{}] *Validation*: Prec@1 {top1.avg:.3f}'.format(epoch, top1=top1_acc))
+        
+        return top1_acc.avg
+        
+    def save_model(self):
+        torch.save(self.model.state_dict(), self.model_path)
+        
+    def check_point(self):
+        # not implemented yet
+        pass
+    
+    def logging(self):
+        # not implemented yet
+        pass
+    
+    
---
+++ utils/data_utils.py
@@ -0,0 +1,42 @@
+import torch
+from torchtext.datasets import AG_NEWS
+from torch.utils.data import DataLoader
+
+def pad_sequence(sequences, batch_first=False, max_len=None, padding_value=0):
+    max_size = sequences[0].size()
+
+class AG_NEWS_DATASET():
+    def __init__(self, tokenizer=None, batch_size=32, max_sen_len =None):
+        self.tokenizer = tokenizer
+        self.specials = ['[UNK]', '[PAD]', '[CLS]']
+        self.batch_size = batch_size
+        self.max_sen_len = 100
+        self.train_ds = AG_NEWS(root='./data', split='train')
+        self.test_ds = AG_NEWS(root='./data', split='test')
+        
+    def generate_batch(self, data_batch):
+        batch_sentence, batch_label = [], []
+        for (lab, sen) in data_batch:
+            batch_sentence.append(sen)
+            batch_label.append(lab)
+           
+        tmp = self.tokenizer(batch_sentence, padding='longest', return_tensors='pt') 
+        
+        batch_sentence = tmp['input_ids']
+        batch_label = torch.tensor(batch_label, dtype=torch.long)
+        attn_mask = tmp['attention_mask']
+        
+        return batch_sentence, batch_label, attn_mask
+    
+    def load_data(self):
+        
+        train_dl = DataLoader(self.train_ds, 
+                              batch_size=self.batch_size, 
+                              shuffle=True,
+                              collate_fn=self.generate_batch)
+        test_dl = DataLoader(self.test_ds,
+                             batch_size=self.batch_size,
+                             shuffle=True,
+                             collate_fn=self.generate_batch)
+        
+        return train_dl, test_dl
